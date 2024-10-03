ec700392318d5a92ef42f774714caa26d6a297ea
wndh98 <wndh98@naver.com>
2024-10-03 16:46:58 +0900

    first commit

---
+++ src/main/java/com/mogumogu/moru/TestController.java
@@ -0,0 +1,14 @@
+package com.mogumogu.moru;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String testController(){
+        return "성공";
+    }
+}
---
+++ src/test/java/com/mogumogu/moru/MoruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mogumogu.moru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
