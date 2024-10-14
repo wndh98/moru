86aded65a1520705e6608635c609724a1217459f
wndh98 <wndh98@naver.com>
2024-10-14 13:09:43 +0900

    chore : editor 리액트 변경
    Test.tsx파일 변경 말고 크게 변경된점 없습니다.

--- src/main/frontend/src/Test.tsx
+++ src/main/frontend/src/Test.tsx
@@ -1,66 +1,83 @@
 import { South } from "@mui/icons-material";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import ReactQuill from "react-quill";
-const modules = {
-    toolbar: {
-        container: [
-            [{ header: [1, 2, 3, 4, 5, false] }],
-            ["bold", { "color": [] }, "link", { "size": ["small", false, "large", "huge"] }, "underline"],
-            ["image"],
-        ],
-        handlers: {
-            image: setEditorImage
-        }
-    }
-};
-const MAX_FILE_SIZE = 1024 * 1024 * 10;
-function setEditorImage() {
-    const fileInput = document.createElement("input");
-    fileInput.setAttribute("type", "file");
-    fileInput.setAttribute('multiple', 'multiple')
-    fileInput.accept = "image/*";
-    fileInput.click();
-    fileInput.addEventListener("change", async function () {
-        if (this.files == null) return;
-        const fileList: FileList = this.files;
-        const formData = new FormData();
-        for (let i = 0; i < fileList.length; i++) {
-            const fileExtension = fileList[i].name.split(".").pop()?.toLowerCase();
-            if (fileExtension == null) {
-                alert("잘못된 파일입니다.");
-                return;
-            }
-            if (!["gif", "jpg", "jpeg", "png", "bmp"].includes(fileExtension)) {
-                alert("지원하지 않는 파일형식입니다.");
-                return
-            }
-            const fileSize = fileList[i].size;
-            if (fileSize > MAX_FILE_SIZE) {
-                alert("업로드 가능한 용량은 10MB입니다.");
-                return;
-            }
-            formData.append("bfFile", fileList[i]);
-        }
-        
-        
-        const response = axios.post("/boardFile/free/1",formData);
-        console.log(response);
-    })
-}
+import Editor from "./components/Editor";
 function Test() {
-
-
-
+    // const modules = {
+    //     toolbar: {
+    //         container: [
+    //             [{ header: [1, 2, 3, 4, 5, false] }],
+    //             ["bold", { "color": [] }, "link", { "size": ["small", false, "large", "huge"] }, "underline"],
+    //             ["image"],
+    //         ],
+    //         handlers: {
+    //             image: setEditorImage
+    //         }
+    //     }
+    // };
+    // const MAX_FILE_SIZE = 1024 * 1024 * 10;
+    
+    // const quillRef = useRef<ReactQuill>();
+    // function setEditorImage() {
+    //     const fileInput = document.createElement("input");
+    //     fileInput.setAttribute("type", "file");
+    //     fileInput.setAttribute('multiple', 'multiple')
+    //     fileInput.accept = "image/*";
+    //     fileInput.click();
+    //     fileInput.addEventListener("change", async function () {
+    //         if (this.files == null) return;
+    //         const fileList: FileList = this.files;
+    //         const formData = new FormData();
+    //         for (let i = 0; i < fileList.length; i++) {
+    //             const fileExtension = fileList[i].name.split(".").pop()?.toLowerCase();
+    //             if (fileExtension == null) {
+    //                 alert("잘못된 파일입니다.");
+    //                 return;
+    //             }
+    //             if (!["gif", "jpg", "jpeg", "png", "bmp"].includes(fileExtension)) {
+    //                 alert("지원하지 않는 파일형식입니다.");
+    //                 return
+    //             }
+    //             const fileSize = fileList[i].size;
+    //             if (fileSize > MAX_FILE_SIZE) {
+    //                 alert("업로드 가능한 용량은 10MB입니다.");
+    //                 return;
+    //             }
+    //             formData.append("bfFile", fileList[i]);
+    //         }
+    
+    
+    //         const response = await axios.post("/boardFile/free/1", formData);
+    //         const bfRealFilenames=response.data;
+    //         const quill = quillRef.current?.getEditor();
+    //         const range = quill?.getSelection();
+    //         if(range!==null && range!== undefined){
+    //             bfRealFilenames.forEach((bfRealFilename:string) => {
+    //                 // quill?.insertEmbed(range.index,"image",`/upload/board/${boNum}/${bfRealFilename}`)    
+    //             });
+                
+    //         }
+    //     })
+    // }
 
     return (
-        <div>
-            <ReactQuill
-                modules={modules}
+        // <div>
+        //     <ReactQuill
 
-            />
-            <button type="button">테스트</button>
-        </div>
+        //         modules={modules}
+        //         ref={(element) => {
+        //             if (element !== null) {
+        //                 quillRef.current = element;
+        //             }
+        //         }}
+        //     />
+        //     <button type="button">테스트</button>
+        // </div>
+        <Editor
+        type="board"
+        num={1}
+        ></Editor>
     )
 }
 export default Test;
